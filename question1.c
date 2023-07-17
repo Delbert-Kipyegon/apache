c2bc96661379769b147ef9938a87e66001f5fb39
Delbert-Kipyegon <delbertkip@gmail.com>
2023-07-17 14:41:21 +0300

    questions well done

--- question1.c
+++ question1.c
@@ -1,94 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum TypeTag
-{
-    ADD,
-    MUL,
-    SUB,
-    DIV,
-    FIBO
+// Enum to identify the type of operation or function to be performed
+typedef enum TypeTag {
+    VALUE, ADD, MUL, SUB, FIB
 } TypeTag;
 
-typedef struct Node
-{
+// Node structure to hold the operation type, value, and pointers to child nodes
+typedef struct Node {
     TypeTag type;
-    int val;
-    struct Node *left;
-    struct Node *right;
+    int value;
+    struct Node* left;
+    struct Node* right;
 } Node;
 
-Node *makeFunc(TypeTag type, Node *left, int val)
-{
-    Node *node = (Node *)malloc(sizeof(Node));
+// Function to create a new node
+Node* makeNode(TypeTag type, int value, Node* left, Node* right) {
+    Node* node = (Node*)malloc(sizeof(Node));
     node->type = type;
-    node->val = val;
-    node->left = left;
-    node->right = NULL;
-    return node;
-}
-
-Node *makeFuncWithNodes(TypeTag type, Node *left, Node *right)
-{
-    Node *node = (Node *)malloc(sizeof(Node));
-    node->type = type;
+    node->value = value;
     node->left = left;
     node->right = right;
     return node;
 }
 
-int fibo(int n)
-{
-    int a = 0, b = 1, c, i;
-    if (n == 0)
-        return a;
-    for (i = 2; i <= n; i++)
-    {
-        c = a + b;
-        a = b;
-        b = c;
+// Dynamic Programming approach to calculate Fibonacci sequence
+int fibonacci(int n) {
+    int fib[n+1];
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
     }
-    return b;
+    return fib[n];
 }
 
-int calc(Node *node)
-{
-    switch (node->type)
-    {
-    case ADD:
-        return (node->right == NULL) ? node->left->val + node->val : node->left->val + node->right->val;
-    case MUL:
-        return (node->right == NULL) ? node->left->val * node->val : node->left->val * node->right->val;
-    case SUB:
-        return (node->right == NULL) ? node->left->val - node->val : node->left->val - node->right->val;
-    case DIV:
-        if ((node->right == NULL && node->val != 0) || (node->right != NULL && node->right->val != 0))
-            return (node->right == NULL) ? node->left->val / node->val : node->left->val / node->right->val;
-        else
-            return 0; // In case of division by zero, returns 0. You might want to add some error handling here.
-    case FIBO:
-        return fibo(node->left->val);
-    default:
-        return 0; // In case of invalid operation, returns 0. You might want to add some error handling here.
+// Function to perform the calculation based on the operation type of the node
+int calc(Node* node) {
+    switch(node->type) {
+        case VALUE:
+            // Value type node, return the value directly
+            return node->value;
+        case ADD:
+            // Addition operation
+            return calc(node->left) + calc(node->right);
+        case MUL:
+            // Multiplication operation
+            return calc(node->left) * calc(node->right);
+        case SUB:
+            // Subtraction operation
+            return calc(node->left) - calc(node->right);
+        case FIB:
+            // Fibonacci sequence
+            return fibonacci(node->value);
+        default:
+            return 0;
     }
 }
 
-int main()
-{
-    Node *add = makeFunc(ADD, makeFunc(ADD, NULL, 10), 6);
-    Node *mul = makeFunc(MUL, makeFunc(MUL, NULL, 5), 4);
-    Node *sub = makeFuncWithNodes(SUB, mul, add);
-    Node *fibo = makeFunc(FIBO, sub, 0);
-
+int main() {
+    // Create 'add' node to perform addition of 10 and 6
+    Node* add = makeNode(ADD, 0, makeNode(VALUE, 10, NULL, NULL), makeNode(VALUE, 6, NULL, NULL));
+    // Create 'mul' node to perform multiplication of 5 and 4
+    Node* mul = makeNode(MUL, 0, makeNode(VALUE, 5, NULL, NULL), makeNode(VALUE, 4, NULL, NULL));
+    // Create 'sub' node to subtract the result of 'mul' from 'add'
+    Node* sub = makeNode(SUB, 0, add, mul);
+    // Create 'fibo' node to get the 4th Fibonacci number
+    Node* fibo = makeNode(FIB, SUB, NULL, NULL);
+    
+    // Print the results of the calculations
     printf("add : %d\n", calc(add));
     printf("mul : %d\n", calc(mul));
     printf("sub : %d\n", calc(sub));
     printf("fibo : %d\n", calc(fibo));
 
-    free(add);
-    free(mul);
-    free(sub);
-    free(fibo);
+    /**
+     * Expected Output:
+     * add: 16
+     * mul: 20
+     * sub: -4 
+     * fibo: 2
+    */
 
     return 0;
 }
--- question2.c
+++ question2.c
@@ -1,92 +1,92 @@
-/**
- * Recursive implementation
- * 
- * Advantages
- * 1. Simplicity: The recursive method is a direct translation of the problem statement.
- * 
- * Disadvantages
- * 1. Inefficiency: The recursive implementation recalculates the same subproblems over and over again.
- * 2. Stack overflow: For large inputs, this approach may lead to stack overflow due to many recursive calls.
- * 
- * **/
-
-#include <stdio.h>
-
-int F_recursive(int n)
-{
-    if (n == 0)
-        return 0;
-    else if (n == 1)
-        return 1;
-    else if (n == 2)
-        return 2;
-    else
-        return F_recursive(n - 3) + F_recursive(n - 2);
-}
-
-/**
- * Memoization/Dynamic Programming
- * 
- * Advantages
- * Efficiency: The DP method remembers results of subproblems to avoid unnecessary recalculation.
- * Avoiding stack overflow: As each subproblem is calculated only once, 
- * this approach avoids the stack overflow issue present in the simple recursive approach.
- * 
- * Disadvantages
- * Space trade-off: The memoization approach uses extra space to store the results of subproblems.
- * **/
-
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Below is the Code the three functions and the main function
+ *
+ * Please find the advantages and disadvantages in the README2.md file
+ */
+
+// Recursive implementation of the recurrence relation
+int F_recursive(int n)
+{
+    if (n == 0) // Base case for n = 0
+        return 0;
+    else if (n == 1) // Base case for n = 1
+        return 1;
+    else if (n == 2) // Base case for n = 2
+        return 2;
+    else
+        // Recursive case for n > 2
+        return F_recursive(n - 3) + F_recursive(n - 2);
+}
+
+// Helper function for the Dynamic Programming implementation
 int F_memo(int n, int *memo)
 {
-    if (memo[n] != -1)
+    if (memo[n] != -1) // If the value has already been calculated, return it
         return memo[n];
     else
     {
+        // Otherwise, calculate it using the recurrence relation
         memo[n] = F_memo(n - 3, memo) + F_memo(n - 2, memo);
-        return memo[n];
+        return memo[n]; // Return the calculated value
     }
 }
 
+// Dynamic Programming implementation of the recurrence relation
 int F_dp(int n)
 {
+    // Allocate memory for the memo array
     int *memo = (int *)malloc((n + 1) * sizeof(int));
+    // Initialize all elements of the memo array to -1
     for (int i = 0; i <= n; i++)
         memo[i] = -1;
+    // Set base cases
     memo[0] = 0;
     memo[1] = 1;
     memo[2] = 2;
+    // Calculate the n-th term using the helper function
     int result = F_memo(n, memo);
+    // Free the memory allocated for the memo array
     free(memo);
+    // Return the result
     return result;
 }
 
-/**
- * Iterative Bottom-Up approach implementation
- * 
- * Advantages
- * Efficiency: Like the DP method, the iterative method calculates each subproblem only once.
- * No extra space: Unlike the DP method, the iterative method does not need extra space proportional to n. 
- * Only a constant amount of space is needed.
- * Disadvantages
- * Less intuitive: The iterative method can be less intuitive to implement than the recursive and DP methods.
- */
-
-#include <stdio.h>
-
+// Iterative implementation of the recurrence relation
 int F_iterative(int n)
 {
+    // Initialize the array for the first three terms
     int F[3] = {0, 1, 2};
 
+    // Calculate each term from 3 to n
     for (int i = 3; i <= n; i++)
     {
+        // Update the array to hold the last two terms
         int temp = F[2];
         F[2] = F[0] + F[1];
         F[0] = F[1];
         F[1] = temp;
     }
 
+    // Return the n-th term
     return F[2];
+}
+
+int main()
+{
+    // Let's compute for n = 5 for all three methods
+    int n = 5;
+
+    // Recursive method
+    printf("Recursive: %d\n", F_recursive(n));
+
+    // Dynamic Programming method
+    printf("Dynamic Programming: %d\n", F_dp(n));
+
+    // Iterative method
+    printf("Iterative: %d\n", F_iterative(n));
+
+    return 0;
 }
